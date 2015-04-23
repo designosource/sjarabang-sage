4bb43dc330838a24bd6f29c6d9b49b3c8fdfe006
Ritchie Jacobs <jacobs.ritchie@gmail.com>
2015-04-23 22:33:35 +0200

    Sage-Sjarabang files

---
+++ wp-content/themes/sjarabang/templates/entry-meta.php
@@ -0,0 +1,3 @@
+<div class="entry-meta">
+  <span>gepost op </span><time class="updated" datetime="<?= get_the_time('c'); ?>"><?= get_the_date(''); ?></time>
+</div>
---
+++ wp-content/themes/sjarabang/templates/header.php
@@ -0,0 +1,28 @@
+<?php use Roots\Sage\Nav\NavWalker; ?>
+
+<header class="banner navbar navbar-default navbar-static-top" role="banner">
+
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only"><?= __('Toggle navigation', 'sage'); ?></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>">
+        <img src="<?php echo get_bloginfo('template_directory');?>/dist/images/logo.png" alt="Sjarabang">
+      </a>
+    </div>
+
+    <nav class="collapse navbar-collapse" role="navigation">
+      <?php
+      if (has_nav_menu('primary_navigation')) :
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new NavWalker(), 'menu_class' => 'nav navbar-nav']);
+      endif;
+      ?>
+    </nav>
+
+  </div>
+  <div class="hero"></div>
+</header>

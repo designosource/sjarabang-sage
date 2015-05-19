28f2199c17e1b9e825c9cabe4afd18b0f4c51823
LisaSmets <lisa_smets@hotmail.com>
2015-05-19 22:10:55 +0200

    Footer incl. adres, site en sociale media

--- wp-content/themes/sjarabang-production/templates/footer.php
+++ wp-content/themes/sjarabang-production/templates/footer.php
@@ -1,5 +1,19 @@
 <footer class="content-info" role="contentinfo">
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
+    <p>Sjarabang vzw</p>
+    <p>Kerkhoflei 47-49</p>
+ <p>2800 Mechelen</p>
+<p>info@sjarabangvzw.be</p>
+  </div>
+  <div class="containerright">
+  	<p>Neem ook eens een kijkje op onze website</p>
+  	<p><a href="http://www.sjarabangvzw.be" class="site">www.sjarabangvzw.be</a></p>
+  	<div class='social'>
+  		<a href="http://www.facebook.com/Sjarabang" target="_blank" class="f"></a>
+  		<a href="http://www.twitter.com/SjarabangVZW" target="_blank" class="t"></a>
+		<a href="http://www.youtube.com/channel/UC4rdrdErkaGK8_ohcCOcTDA" target="_blank" class="y"></a>
+  	</div>
+  
   </div>
 </footer>

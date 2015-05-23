6fbd6266a281fda1a5084e7fb864240b6f73893b
Ritchie Jacobs <jacobs.ritchie@gmail.com>
2015-05-23 16:59:35 +0200

    add 'Made by' to footer

--- wp-content/themes/sjarabang-production/templates/footer.php
+++ wp-content/themes/sjarabang-production/templates/footer.php
@@ -3,17 +3,19 @@
     <?php dynamic_sidebar('sidebar-footer'); ?>
     <p>Sjarabang vzw</p>
     <p>Kerkhoflei 47-49</p>
- <p>2800 Mechelen</p>
-<p>info@sjarabangvzw.be</p>
+    <p>2800 Mechelen</p>
+    <p>info@sjarabangvzw.be</p>
   </div>
   <div class="containerright">
-  	<p>Neem ook eens een kijkje op onze website</p>
-  	<p><a href="http://www.sjarabangvzw.be" class="site">www.sjarabangvzw.be</a></p>
-  	<div class='social'>
-  		<a href="http://www.facebook.com/Sjarabang" target="_blank" class="f"></a>
-  		<a href="http://www.twitter.com/SjarabangVZW" target="_blank" class="t"></a>
-		<a href="http://www.youtube.com/channel/UC4rdrdErkaGK8_ohcCOcTDA" target="_blank" class="y"></a>
-  	</div>
-  
+    <p>Neem ook eens een kijkje op onze website</p>
+    <p><a href="http://www.sjarabangvzw.be" class="site">www.sjarabangvzw.be</a></p>
+    <div class='social'>
+      <a href="http://www.facebook.com/Sjarabang" target="_blank" class="f"></a>
+      <a href="http://www.twitter.com/SjarabangVZW" target="_blank" class="t"></a>
+      <a href="http://www.youtube.com/channel/UC4rdrdErkaGK8_ohcCOcTDA" target="_blank" class="y"></a>
+    </div>
+  </div>
+  <div class="madeby">
+    <p>Website door <a href="http://www.designosource.be">designosource</a></p>
   </div>
 </footer>
